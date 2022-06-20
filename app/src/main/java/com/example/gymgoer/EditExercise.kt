34627622f5dc389326cfe2cd8b8f0d811e0b15b3
Johan Hörnblad <johan.hornblad@acino.se>
2022-06-20 16:34:02 +0200

    creats list and changes activity

---
+++ app/src/main/java/com/example/gymgoer/EditExercise.kt
@@ -0,0 +1,11 @@
+package com.example.gymgoer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditExercise : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_exercise)
+    }
+}
--- app/src/main/java/com/example/gymgoer/MainActivity.kt
+++ app/src/main/java/com/example/gymgoer/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.gymgoer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.listLView)
+        val names = arrayOf("DeadLift", "Squats", "Ohp")
+        val arrayAdapter :ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, names)
+        listView.adapter = arrayAdapter
+        val intent: Intent = Intent(this, EditExercise::class.java)
+        listView.setOnItemClickListener{adapterView, view, i ,l ->
+            startActivity(intent)
+            Toast.makeText(this, "Item Selected " + names[i], Toast.LENGTH_LONG).show()}
     }
 }
